516e16d781098f4ad0d68c3e408c614c4d800a30
aafanasyevaa <afanasyevaa.anastasiia@gmail.com>
2021-11-05 17:54:03 +0300

    renamed the class

--- src/test/java/aafanasyevaa/SelenideTest.java
+++ src/test/java/aafanasyevaa/SelenideTest.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class GitHubTest {
+public class SelenideTest {
 
     @BeforeAll
     static void beforeAll() {
